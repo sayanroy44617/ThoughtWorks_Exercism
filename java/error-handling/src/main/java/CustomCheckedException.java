39f6b6e9be18128349f6bf6ddfeab82476a5b48a
Sayan Roy <sayanroy44617@gmail.com>
2024-05-06 16:05:43 +0530

    error-handling exercise

---
+++ java/error-handling/src/main/java/CustomCheckedException.java
@@ -0,0 +1,7 @@
+class CustomCheckedException extends Exception {
+    CustomCheckedException() {
+    }
+    CustomCheckedException(String message) {
+        super(message);
+    }
+}
---
+++ java/error-handling/src/main/java/CustomUncheckedException.java
@@ -0,0 +1,7 @@
+class CustomUncheckedException extends RuntimeException {
+    CustomUncheckedException() {
+    }
+    CustomUncheckedException(String message) {
+        super(message);
+    }
+}
